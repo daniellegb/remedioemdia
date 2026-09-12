f7887be9903cd02b0ab24fda0120ebbbd159022a
Danielle Balogh <daniellegb@protonmail.com>
2026-09-11 23:11:47 -0300

    Deleção do subscribe para CTA

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -325,7 +325,7 @@ const Login: React.FC = () => {
                 isSignUp ? 'text-blue-600 font-black' : 'text-slate-500 hover:text-slate-800'
               }`}
             >
-              Cadastre-se grátis
+              Cadastrar
               {isSignUp && (
                 <motion.div
                   layoutId="activeTabIndicator"
@@ -479,7 +479,7 @@ const Login: React.FC = () => {
                 transition={{ duration: 0.15 }}
                 className="flex items-center justify-center gap-2 w-full"
               >
-                {loading ? <Loader2 className="animate-spin" size={24} /> : (isForgotPass ? 'Enviar e-mail' : (isSignUp ? 'Cadastre-se grátis' : 'Entrar'))}
+                {loading ? <Loader2 className="animate-spin" size={24} /> : (isForgotPass ? 'Enviar e-mail' : (isSignUp ? 'Cadastrar' : 'Entrar'))}
               </motion.span>
             </AnimatePresence>
           </button>
